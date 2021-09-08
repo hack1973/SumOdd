82ae13229d84160f05b8c53f187f1830e4ef5a3e
Michael Hacker <hack1973@swbell.net>
2021-09-08 04:38:18 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import static com.company.SumOddRange.sumOdd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(SumOddRange.sumOdd(1, 100));
+        System.out.println(SumOddRange.sumOdd(-1, 100));
+        System.out.println(SumOddRange.sumOdd(100, 100));
+        System.out.println(SumOddRange.sumOdd(13, 13));
+        System.out.println(SumOddRange.sumOdd(100, -100));
+        System.out.println(SumOddRange.sumOdd(100, 1000));
+    }
+}
---
+++ src/com/company/SumOddRange.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class SumOddRange {
+    public static boolean isOdd(int number) {
+        if (number <= 0) {
+            return false;
+        }
+        if (number % 2 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int sumOdd(int start, int end) {
+        if (end < start || start <= 0) {
+            return -1;
+        }
+        int oddSum = 0;
+        for (int i = start;i <= end;i++) {
+            if (isOdd(i)) {
+                oddSum += i;
+            }
+        }
+        return oddSum;
+    }
+}
+
+
